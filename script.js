387d430c167d4ce15caf7a5d50564fc767eb234b
indezah <64645496+indezah@users.noreply.github.com>
2023-06-10 04:22:29 +0530

    Basic Functionality

--- script.js
+++ script.js
@@ -1,24 +1,97 @@
-let buttons = document.getElementsByClassName('button');
 
 let operands = [];
 let operators = [];
-let temp = ''
+let counter = 0;
+
+let isOperatorSet = false;
+let isContinue = false;
+
+let temp = '';
+
+let buttons = document.getElementsByClassName('button');
+
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', () => { load(buttons[i].getAttribute('data-no')) });
+    buttons[i].addEventListener('click', () => {
+        load(buttons[i].getAttribute('data-no'))
+    });
+}
+
+function solve(operand1, operand2, operators) {
+
 }
 
 function load(val) {
-    if (typeof Number(val) == 'number') {
-        showDisplay(val);
-        operands.push(val)
-        console.log(operands)
-        return
+
+    if (val === 'plus' || val === 'minus' || val === 'mul' || val === 'div') {
+        if (isOperatorSet === false) {
+            {
+                if (temp != '') {
+                    operands.push(temp);
+                }
+                isOperatorSet = true;
+                temp = '';
+                operators.push(val);
+                console.log(operands);
+
+            }
+        }
+
+
+
+    } else if (val === 'clear') {
+        operands = [];
+        operators = [];
+        temp = '';
+        isOperatorSet = false;
+        showDisplay('0');
+
+    } else if (val === 'equals') {
+        console.log(operands);
+        operands.push(temp);
+        temp = '';
+        while (operators.length > 0) {
+            i = 0
+            if (operators[i] === 'plus') {
+                operands[i] = Number(operands[i]) + Number(operands[i + 1]);
+            }
+            else if (operators[i] === 'minus') {
+                operands[i] = Number(operands[i]) - Number(operands[i + 1]);
+            }
+            else if (operators[i] === 'mul') {
+                operands[i] = Number(operands[i]) * Number(operands[i + 1]);
+            }
+            else if (operators[i] === 'div') {
+                if (operands[i + 1] === '0') {
+                    showDisplay('Error');
+                    return;
+                }
+                operands[i] = Number(operands[i]) / Number(operands[i + 1]);
+            }
+            operands.splice(i + 1, 1);
+            operators.splice(i, 1);
+
+            console.log(operands);
+        }
+        showDisplay(operands[0]);
+
+
+
+    } else {
+        temp = temp + val;
+        isOperatorSet = false;
+        showDisplay(temp);
+
+
     }
-    console.log('operand');
+
+    // console.log(operands);
+    // console.log(operators);
+
 }
 
 
 function showDisplay(val) {
+
     document.getElementsByClassName('cal-display')[0].innerHTML = val;
 }
 
